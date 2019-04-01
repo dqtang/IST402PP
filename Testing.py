35e146627e2a9b9af2a667d633bdb8e567bbe23c
Dennis Tang <dqt5211@psu.edu>
2019-04-01 19:47:19 -0400

    Pseudo Code Added

--- Testing.py
+++ Testing.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+#Set local host.
 browser.get('http://localhost:8000')
 
+
+#Set List title
 assert 'Class Requirement List' in browser.title
 
 
-
+#Close Browser.
 browser.quit()
 
