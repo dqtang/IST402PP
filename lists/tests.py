449d30b08e09941f825031b2016723e3dcbb72d0
Dennis Tang <dqt5211@psu.edu>
2019-04-10 22:51:10 -0400

    Home Page Template

--- lists/tests.py
+++ lists/tests.py
@@ -2,19 +2,13 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_url_resolves_home_page(self):
-        found = resolve('/') 
-        self.assertEqual(found.func, home_page)  
-
 
     def test_home_page_correct(self):
-            request = HttpRequest()  
-            response = home_page(request)  
-            html = response.content.decode('utf8')  
-            self.assertTrue(html.startswith('<html>'))  
-            self.assertIn('<title>Course Management</title>', html)  
-            self.assertTrue(html.endswith('</html>')) 
+            
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
