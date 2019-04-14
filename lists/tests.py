ba6267eb78ec4db43c374751640a7623ccf7b3b9
Jiawen <jxchen917@gmail.com>
2019-04-14 18:07:47 -0400

    Spelling errors fix

--- lists/tests.py
+++ lists/tests.py
@@ -24,7 +24,7 @@ class NewListTest(TestCase):
 
   
 
-    def test_redirecct_after_POST(self):
+    def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'course_text': 'A new Course'})
         self.assertRedirects(response, '/lists/best-course-list/')
 
