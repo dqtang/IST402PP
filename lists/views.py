db8318a698a2c665ab013c5f451f8ed565df6ccc
Dennis Tang <dqt5211@psu.edu>
2019-04-14 17:40:47 -0400

    Forgot files v2

--- lists/views.py
+++ lists/views.py
@@ -7,18 +7,11 @@ from lists.models import Courses, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    courses = Courses.objects.filter(list=list_)
+def view_list(request):
+    courses = Courses.objects.all()
     return render(request, 'list.html', {'courses' : courses})
-
-def add_course(request, list_id):
-    list_ = List.objects.create()
-    Courses.objects.create(text=request.POST['course_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
-
 
 def new_list(request):
     list_ = List.objects.create()
     Courses.objects.create(text=request.POST['course_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+    return redirect('/lists/best-course-list/')
