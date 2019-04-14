9ee733eb8afefbf90e71f85129249b2e170c544b
Dennis Tang <dqt5211@psu.edu>
2019-04-14 18:33:06 -0400

    Simplified URLS

--- lists/tests.py
+++ lists/tests.py
@@ -24,7 +24,7 @@ class NewListTest(TestCase):
 
   
 
-    def test_redirecct_after_POST(self):
+    def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'course_text': 'A new Course'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
