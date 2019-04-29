fdcda12b2c5078e06c30190141a91bdc21f0cc93
dqtang <dqt5211@psu.edu>
2019-04-28 22:27:58 -0400

    Changed Comments

--- running_tests/tests.py
+++ running_tests/tests.py
@@ -33,18 +33,19 @@ class FIrstTestCase(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url)
      
      
-    #Danny wants a list e manage the courses he requires for his major.
+    #Danny wants a list to manage the courses he requires for his major.
     #He goes to this website that he heard from his senior to help manage his course.
 
     #Danny saw the title of the website and knows he is on the right site.
 
     #1) He saw a textbox for him to enter his course.
-        #submitBox = self.browser.find_element_by_xpath('//button[contains(text(), "Submit")]').click
         inputbox = self.browser.find_element_by_id('Course_Id')
+
+
+    #2) He enters a course as a test and press es the enter key and saw the list updated. Adding his course to the list
+        
         inputbox.send_keys('History 101')
 
-        #submitBox()
-        #time.sleep(5)
         inputbox.send_keys(Keys.ENTER)
         
         #CourseAttribute = inputbox.get_attribute()
@@ -55,51 +56,26 @@ class FIrstTestCase(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_table('2: Language 101')
 
+    #3) His Course shows up and when he clicks on the checkbox it crossed out the selected course and 
+    # the text turned green. 
         checkbox = self.browser.find_element_by_id('Completed')
         checkbox.click()
-        # time.sleep(5)
+        time.sleep(5)
 
 
-    #2) He enters a course as a test and press es the enter key and saw the list updated. Adding his course to the list
-        #Still need to work on how to get user input Sprint 2
-        
 
-        #Testing phase allow time to enter info
-
-        """
-        Gets text from input box used for formating currently useless
-        CourseAdding = inputbox.text
-        """
-
-        """
-        Sprint 2: Make it not Hardcoded 
-        Sample: Currently Gives AssertionError from home.html
-        self.check_for_row_in_table('1: {0}'.format(CourseAdding))
-        """
-
-
-    #2a)     
-
-    #3) Sprint 2: He noticed that there is options next to his course list that sets it to done or in-progress. 
-
-    #4) When Danny selected done, a green text that says "Done" pops up next to the course.
-
-    #5) If he select "In-Progress" a yellow text pops up that says "In-Progress"
-
-    #6) Excited he wonders if the list will save and be used later on.
-
-
-    #7) Danny notices the Unqiue URL
+    #4) Danny notices the Unqiue URL
         danny_list_url = self.browser.current_url
         self.assertRegex(danny_list_url, '/lists/.+')
 
         time.sleep(5)
-    #8) His friend Josh decided to give the site a try
+
+    #5) His friend Josh decided to give the site a try
     ##New browser session to avoid having another user info display
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-    #9)Josh vists homepage
+    #6)Josh vists homepage
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('History 101', page_text)
@@ -108,7 +84,8 @@ class FIrstTestCase(StaticLiveServerTestCase):
         inputbox.send_keys('Art 101')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_table('1: Art 101')
-    #10) Josh noticed his own unique url tooss
+
+    #7) Josh noticed his own unique url tooss
         josh_list_url = self.browser.current_url
         self.assertRegex(josh_list_url, '/lists/.+')
         self.assertNotEqual(danny_list_url, josh_list_url)
